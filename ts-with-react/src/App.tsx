7b2c70199ab5ad0b86c41a60e439d3cb96c332ea
kcshan <cshan.kong@gmail.com>
2020-11-15 23:38:22 +0800

    fix: HOC

--- ts-with-react/src/App.tsx
+++ ts-with-react/src/App.tsx
@@ -1,24 +1,35 @@
 import React, { useState } from 'react';
 import LikeButton from './components/LikeButton'
-// import MouseTracker from './components/MouseTracker'
-import useMousePosition from './hooks/useMoussePosition'
+import MouseTracker from './components/MouseTracker'
+import WithLoader from './components/WithLoader'
 import './App.css';
+
+interface IShowResoult {
+  message: string;
+  status: string;
+}
+
+const DogShow: React.FC<{data: IShowResoult}> = ({ data }) => {
+  return (
+    <>
+      <h2>Dog show: {data.status}</h2>
+      <img src={data.message}/>
+    </>
+  )
+}
 
 function App() {
   const [show, setShow] = useState(true)
-  const positions = useMousePosition()
+  const WrappedDogShow = WithLoader(DogShow, 'https://dog.ceo/api/breeds/image/random')
   return (
     <div className="App">
       <header className="App-header">
         <p>
           <button onClick={() => { setShow(!show) }}>Toggle Tracker</button>
         </p>
+        <WrappedDogShow/>
         <LikeButton/>
-        {/* { show && <MouseTracker/> } */}
-        <p>
-          X: {positions.x},
-          Y: {positions.y}
-        </p>
+        { show && <MouseTracker/> }
       </header>
     </div>
   );
