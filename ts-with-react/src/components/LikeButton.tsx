ddc9914a2d17cacd96974cb0c40555ae1a5d9e05
kcshan <cshan.kong@gmail.com>
2020-11-18 08:23:34 +0800

    fix: useRef

--- ts-with-react/src/components/LikeButton.tsx
+++ ts-with-react/src/components/LikeButton.tsx
@@ -1,32 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import useMousePosition from '../hooks/useMoussePosition'
+import React, { useState, useEffect, useRef } from 'react'
 
 const LikeButton: React.FC = () => {
   const [like, setLike] = useState(0)
   const [on, setOn] = useState(true)
-  const positions = useMousePosition()
+  const likeRef = useRef(0)
+  const didMountRef = useRef(false)
+  const domRef = useRef<HTMLInputElement>(null)
   useEffect(() => {
     console.log('document title effect is running')
     document.title = `点击了${like}次`
   }, [like])
+  useEffect(() => {
+    if (didMountRef.current) {
+      console.log('this is updated')
+    } else {
+      didMountRef.current = true
+    }
+  })
+  useEffect(() => {
+    if (domRef && domRef.current) {
+      domRef.current.focus()
+    }
+  })
   function handleAlertClick() {
     setTimeout(() => {
-      alert('you clicked on ' + like)
+      alert('you clicked on ' + likeRef.current)
     }, 3000)
   }
   return (
     <>
-      <button onClick={() => {setLike(like + 1)}}>
+      <input type="text" ref={domRef}/>
+      <button onClick={() => {setLike(like + 1); likeRef.current++}}>
         {like} 👍
       </button>
       <button onClick={() => {setOn(!on) }}>
         {on ? 'ON' : 'OFF'}
       </button>
       <button onClick={handleAlertClick}>Alert</button>
-      <p>
-        X: {positions.x},
-        Y: {positions.y}
-      </p>
     </>
   )
 }
