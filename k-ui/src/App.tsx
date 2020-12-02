119d329d79c3fc7983ee0adb8cff3fa7b9ebb280
kcshan <cshan.kong@gmail.com>
2020-12-02 08:57:40 +0800

    Button

--- k-ui/src/App.tsx
+++ k-ui/src/App.tsx
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <Button disabled>Hello World</Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello World</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Hello World</Button>
         <Button btnType={ButtonType.Link} href="https://www.baidu.com" disabled>百度 Link</Button>
+        <Button btnType={ButtonType.Link} href="https://www.baidu.com">百度 Link</Button>
       </header>
     </div>
   );
